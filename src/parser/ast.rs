cf7c65655ab95f39c1aca47ba5612c1a15b131d6
Aurora <ender@thefluegges.com>
2021-10-07 21:13:06 -0400

    Working on AST

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -3,7 +3,10 @@ use super::super::lexer::Lexer;
 pub fn parse<S: Into<String>>(input: S) {
     let lexer = Lexer::new(input);
 
+    // This iterater handles the first pass.
     for item in lexer {
         
     }
 }
+
+// pub struct 
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,2 +1,18 @@
+use crate::lexer::Lexer;
+
 pub mod structures;
 pub mod ast;
+
+pub struct Parser {
+    pub(crate) lexer: Lexer,
+}
+
+impl Parser {
+    pub fn new(lexer: Lexer) {
+        Parser { lexer };
+    }
+
+    pub fn bind(&self) {
+        
+    }
+}
